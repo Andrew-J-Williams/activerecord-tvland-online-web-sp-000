2d6a7557a0d4d0e302676ff04d3c419d630209e1
Andrew Williams <1williamsaj@gmail.com>
2019-12-05 03:37:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
-    self.characters.name if self.id == characters.show_id
+    self.characters.name if self.id == self.show_id
   end
 
 end
